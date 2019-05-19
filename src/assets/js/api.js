0b47627e856a7c2ea22ea5d65712171e196d5c61
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2019-05-19 23:50:37 +0900

    [update]requestAjax関数の編集

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,16 +1,14 @@
-(function() {
-    var requestAjax = function(endpoint, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
+function requestAjax(endpoint, callback) {
+    var xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
                 callback(this.response);
-            }
-        };
-        xhr.responseType = 'json';
-        xhr.open('GET', endpoint, true);
-        xhr.send();
-    };
-})();
+    }
+    // };
+    xhr.responseType = 'json';
+    xhr.open('GET', endpoint, true);
+    xhr.send();
+}
 
 requestAjax("localhost:8080/api/v1/recipe/1", function(response) {
 
