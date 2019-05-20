fedfd6b3012a8ef8c3eff266a2d2eb86facbb3c9
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2019-05-20 13:23:09 +0900

    [update]apiRequest.jsファイルの作成,login.htmlのエラー修正

---
+++ src/assets/js/apiRequest.js
@@ -0,0 +1,14 @@
+var request = new XMLHttpRequest();
+
+request.open('POST', 'http://localhost:8080/api/v1/auth/login', true);
+request.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8')
+request.responseType = 'json';
+
+request.onload = function() {
+    var data = this.response;
+    console.log(data);
+};
+
+let api_response = request.send('user_id=kmyan&password=opai1919');
+
+document.write(api_response);
