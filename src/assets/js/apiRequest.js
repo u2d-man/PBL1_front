a4d0d045eb758993716128383a405cf8ead90cb4
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2019-05-31 14:27:29 +0900

    [update]login画面の編集

--- src/assets/js/apiRequest.js
+++ src/assets/js/apiRequest.js
@@ -12,7 +12,7 @@ function get_need_response(method, endpoint, column, parameters, callback) {
         response_from_database = this.response;
         // ステータスコードによって処理を変更
         if (request.status === 401) {
-            alert("");
+            alert("IDかパスワードが違います");
             console.log(response_from_database);
         } else if (request.status === 200) {
             choice_data = response_from_database[column];
