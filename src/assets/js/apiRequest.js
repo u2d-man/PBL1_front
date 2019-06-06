d644455b258eded18912cce2f544e7d0ef60a399
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2019-06-06 16:21:57 +0900

    [update]apiRequest.jsの関数名変更と中身の更新

--- src/assets/js/apiRequest.js
+++ src/assets/js/apiRequest.js
@@ -1,30 +1,51 @@
-function get_need_response(method, endpoint, key, parameters, callback) {
+function apiResponse(method, endpoint, key, parameters, callback) {
     // XMLHttpRequestコンストラクタを作成
     var request = new XMLHttpRequest();
 
+    // ハンドラーの登録
+    request.onreadystatechange = function () {
+        switch(request.readyState) {
+            case 0:
+                // 未初期状態
+                console.log("Not initialized ... ");
+                break;
+            case 1:
+                // データ送信中
+                console.log("Sending data ... ");
+                break;
+            case 2:
+                // 応答待ち
+                console.log("Waiting for response ... ");
+                break;
+            // データ受信中
+            case 3:
+                console.log("Receiving of data ... ");
+                break;
+            case 4:
+                // データ受信完了
+                if (request.status == 200 || request.status === 304) {
+                    var response_data = this.response;
+                    if (key === '') {
+                        console.log("Complete!!\n" + response_data);
+                        callback(response_data);
+                    } else {
+                        console.log("Complete!!\n" + response_data[key]);
+                        // コールバック
+                        callback(response_data[key]);
+                    }
+                } else {
+                    console.log( 'Failed. HttpStatus: ' + request.statusText)
+                }
+                break;
+        }
+    };
+
     // Httpリクエストに必要な情報
-    request.open(method, endpoint);
+    request.open(method, endpoint, true);
     request.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
     request.responseType = 'json';
-
-    // 実行
-    request.onload = function () {
-        var response_from_database = this.response;
-        // ステータスコードによって処理を変更
-        if (request.status === 401) {
-            alert("リクエストデータが見つかりません");
-            console.log(response_from_database);
-        } else if (key === '') {
-            console.log(response_from_database);
-            callback(response_from_database);
-        } else {
-            var choice_data = response_from_database[key];
-            console.log(choice_data);
-            callback(choice_data);
-        }
-
-    };
 
     // Httpリクエストを送信(引数指定で特定の情報のみを送信)
     request.send(parameters);
 }
+
